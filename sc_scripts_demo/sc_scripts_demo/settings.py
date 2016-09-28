e33773307809555a8eebdca5484e103358741ab1
Elias Dorneles <eliasdorneles@gmail.com>
2016-09-28 11:19:43 -0300

    starting sc_scripts_demo

---
+++ sc_scripts_demo/sc_scripts_demo/settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+BOT_NAME = 'sc_scripts_demo'
+
+SPIDER_MODULES = ['sc_scripts_demo.spiders']
+NEWSPIDER_MODULE = 'sc_scripts_demo.spiders'
+
+USER_AGENT = 'sc_scripts_demo (http://scrapinghub.com)'
+
+ROBOTSTXT_OBEY = True
