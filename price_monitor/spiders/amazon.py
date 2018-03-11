b853d797356e4154f7d88fc3fdf412da389bf938
rafrox <37214120+rafrox@users.noreply.github.com>
2018-03-12 00:09:20 +1100

    Update amazon.py

--- price_monitor/spiders/amazon.py
+++ price_monitor/spiders/amazon.py
@@ -6,11 +6,10 @@ class AmazonSpider(BaseSpider):
 
     def parse(self, response):
         for product in response.css(".view-advanced-catalog tr > td"):
-
-        item = {}
-        item['title'] = product.css(".views-field-title a ::text").extract_first()
-        item['price'] = product.css(".views-field-phpcode span span::text").extract()[1]
-        item['url'] = product.css(".views-field-title a::attr(href)").extract()
+            item = {}
+            item ['title'] = product.css(".views-field-title a ::text").extract_first()
+            item ['price'] = product.css(".views-field-phpcode span span::text").extract()[1]
+            item ['url'] = product.css(".views-field-title a::attr(href)").extract()
         yield item
 
     next_page = response.css('li.pager-nexta::attr(href)').extract_first()
