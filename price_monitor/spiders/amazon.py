b96d23ec7caf4db32d955caa717df0c2d51689ae
rafrox <37214120+rafrox@users.noreply.github.com>
2018-03-12 00:17:14 +1100

    Update amazon.py

--- price_monitor/spiders/amazon.py
+++ price_monitor/spiders/amazon.py
@@ -11,8 +11,3 @@ class AmazonSpider(BaseSpider):
             item ['price'] = product.css(".views-field-phpcode span span::text").extract()[1]
             item ['url'] = product.css(".views-field-title a::attr(href)").extract()
         yield item
-
-    next_page = response.css('li.pager-nexta::attr(href)').extract_first()
-    if next_page is not None:
-        next_page = response.urljoin(next_page)
-        
