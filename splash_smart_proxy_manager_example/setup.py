2f2605a3b6d1d3e54fdda81b76bbc15a0287f63f
Shashank Kumar <abc@shanky.xyz>
2021-06-24 09:22:28 +0530

    [splash_smart_proxy_manager_example] Minor fix

--- splash_smart_proxy_manager_example/setup.py
+++ splash_smart_proxy_manager_example/setup.py
@@ -5,6 +5,6 @@ setup(
     name = 'project',
     version = '1.0',
     packages = find_packages(),
-    package_data = {'splash_crawlera_example': ['scripts/*.lua',]},
-    entry_points = {'scrapy': ['settings = splash_crawlera_example.settings']},
+    package_data = {'splash_smart_proxy_manager_example': ['scripts/*.lua',]},
+    entry_points = {'scrapy': ['settings = splash_smart_proxy_manager_example.settings']},
 )
