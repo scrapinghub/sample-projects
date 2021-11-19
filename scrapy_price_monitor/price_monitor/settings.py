25fe0e14d91ae14595e27f56027c6f42e36f726f
further-reading <roy.healy87+gh@gmail.com>
2021-11-19 13:28:16 +0000

    Removing placeholder project id

--- scrapy_price_monitor/price_monitor/settings.py
+++ scrapy_price_monitor/price_monitor/settings.py
@@ -6,8 +6,8 @@ NEWSPIDER_MODULE = 'price_monitor.spiders'
 
 # if you want to run it locally, replace None with your scrapy cloud API key
 SHUB_KEY = None
-# if you want to run it locally, replace '999999' by your Scrapy Cloud project ID
-SHUB_PROJ_ID = os.getenv('SHUB_JOBKEY', '999999').split('/')[0]
+# if you want to run it locally, replace PROJ_ID by your Scrapy Cloud project ID
+SHUB_PROJ_ID = os.getenv('SHUB_JOBKEY', 'PROJ_ID').split('/')[0]
 
 ITEM_PIPELINES = {
     'price_monitor.pipelines.CollectionStoragePipeline': 400,
