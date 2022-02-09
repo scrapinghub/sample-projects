23bc45a72d97fe2b487ab837dcda44e20fcf311e
Mukthy <54277855+mukthy@users.noreply.github.com>
2022-02-10 03:11:36 +0530

    Updated settings.py with Zyte Smart Proxy Manager.
    Replaced the CRAWLERA_API to ZYTE_SMARTPROXY_APIKEY, also added the middleware 'scrapy_zyte_smartproxy.ZyteSmartProxyMiddleware': 610

--- splash_smart_proxy_manager_example/splash_smart_proxy_manager_example/settings.py
+++ splash_smart_proxy_manager_example/splash_smart_proxy_manager_example/settings.py
@@ -12,9 +12,13 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy_splash.SplashCookiesMiddleware': 723,
     'scrapy_splash.SplashMiddleware': 725,
     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+    'scrapy_zyte_smartproxy.ZyteSmartProxyMiddleware': 610
 }
 
-CRAWLERA_APIKEY = ''  # Your crawlera API key
+ZYTE_SMARTPROXY_ENABLED = True
+ZYTE_SMARTPROXY_APIKEY = 'API-KEY'
+
+#CRAWLERA_APIKEY = ''  # Your crawlera API key
 
 # Splash settings
 SPLASH_URL = ''  # Splash instance URL from Scrapy Cloud
